14166622d53ea9063ba4cb7fc8bf606319b1a023
Amir Shirif <31713018+amshirif@users.noreply.github.com>
2023-05-12 14:48:24 -0500

    Create contract support

--- src/simple_transactions.rs
+++ src/simple_transactions.rs
@@ -8,44 +8,55 @@ use eyre::{ContextCompat, Result};
 use hex::ToHex;
 
 #[tokio::main]
-async fn main() -> Result<()>{
-    let mnemonic = "pear eye alley dumb online dizzy wine turtle need biology slight frame";
+async fn main() -> Result<()> {
+    // Spawn a ganache instance
+    let mnemonic = "gas monster ski craft below illegal discover limit dog bundle bus artefact";
     let ganache = Ganache::new().mnemonic(mnemonic).spawn();
     println!("HTTP Endpoint: {}", ganache.endpoint());
 
+    // Get the first wallet managed by ganache
     let wallet: LocalWallet = ganache.keys()[0].clone().into();
     let first_address = wallet.address();
-
     println!(
-        "wallet first address: {}", first_address.encode_hex::<String>()
+        "Wallet first address: {}",
+        first_address.encode_hex::<String>()
     );
 
+    // A provider is an Ethereum JsonRPC client
     let provider = Provider::try_from(ganache.endpoint())?.interval(Duration::from_millis(10));
 
+    // Query the balance of our account
     let first_balance = provider.get_balance(first_address, None).await?;
-    println!("wallet first address balance: {}", first_balance);
+    println!("Wallet first address balance: {}", first_balance);
 
-    // let dawnstar_address_hex = "0x7FbFe2FC3c77bcCAA37fbC00EBAB2fb059Ab9bc2";
-    let dawnstar_address = "0x7FbFe2FC3c77bcCAA37fbC00EBAB2fb059Ab9bc2".parse::<Address>()?;
-    // let dawnstar_balance = provider.get_balance(dawnstar_address, None);
+    // Query the blance of some random account
+    let other_address_hex = "0xaf206dCE72A0ef76643dfeDa34DB764E2126E646";
+    let other_address = "0xaf206dCE72A0ef76643dfeDa34DB764E2126E646".parse::<Address>()?;
+    let other_balance = provider.get_balance(other_address, None).await?;
+    println!(
+        "Balance for address {}: {}",
+        other_address_hex, other_balance
+    );
 
-    let tx = TransactionRequest::pay(dawnstar_address, U256::from(1000u64)).from(first_address);
-
+    // Create a transaction to transfer 1000 wei to `other_address`
+    let tx = TransactionRequest::pay(other_address, U256::from(1000u64)).from(first_address);
+    // Send the transaction and wait for receipt
     let receipt = provider
         .send_transaction(tx, None)
         .await?
-        .log_msg("pending transfer")
-        .confirmations(1)
+        .log_msg("Pending transfer")
+        .confirmations(1) // number of confirmations required
         .await?
-        .context("Missing reciept")?;
+        .context("Missing receipt")?;
 
-    println!("transaction mined in block {}",
-        receipt.block_number.context("cannot get block number")?
+    println!(
+        "TX mined in block {}",
+        receipt.block_number.context("Can not get block number")?
     );
-
-    println!("balance of {} {}",
-        dawnstar_address, 
-        provider.get_balance(dawnstar_address, None).await?
+    println!(
+        "Balance of {} {}",
+        other_address_hex,
+        provider.get_balance(other_address, None).await?
     );
 
     Ok(())
